1dd7e5d8026e17ff86a50d58d051a91d6314d84d
prototypeProto <anunfoundx@gmail.com>
2026-04-07 00:18:58 -0400

    backend: split endpoint for separation of concerns

--- backend/src/admin/admin_routes.py
+++ backend/src/admin/admin_routes.py
@@ -87,14 +87,48 @@ async def update_user_role(
         )
 
 
-@router.post("/user/{username}", response_model=Union[RejectedUserRead, UserRead])
-async def judge_pending_user(
-    username: str, session: SessionDependency, token_details: dict = access_token_bearer, approve: bool = Query(False)
+@router.post("/users/{username}/approve", response_model=UserRead)
+async def approve_pending_user(
+    username: str, session: SessionDependency, token_details: dict = access_token_bearer
 ):
     """
-    Admin grants/rejects access to the website to a pending user
-    NOTE: Copies pending_user entry to user table and then deletes the pending_user entry if approved
-            else copied to rejected user and deletes pending_user
+    Admin grants access to the website to a pending user
+    NOTE: Copies pending_user entry to user table and then deletes the pending_user entry
+    """
+    if not await admin_service.verify_admin(token_details, session):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Insufficient permissions"
+        )
+
+    if await admin_service.is_verified_user(username, session):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Failed to update perms. User is already verified",
+        )
+
+    new_user = None
+    try:
+        new_user = await admin_service.approve_pending_user(username, session)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Failed to delete unverified user",
+        )
+    if new_user is None:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Failed to update perms"
+        )
+
+    return new_user
+
+
+@router.post("/users/{username}/reject", response_model=RejectedUserRead)
+async def reject_pending_user(
+    username: str, session: SessionDependency, token_details: dict = access_token_bearer
+):
+    """
+    Admin rejects access to the website to a pending user
+    NOTE: Copies pending_user entry to rejected user and deletes pending_user
     """
     if not await admin_service.verify_admin(token_details, session):
         raise HTTPException(
@@ -120,18 +154,3 @@ async def judge_pending_user(
                 status_code=status.HTTP_404_NOT_FOUND, detail="Pending user not found"
             )
         return rejected
-
-    new_user = None
-    try:
-        new_user = await admin_service.approve_pending_user(username, session)
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Failed to delete unverified user",
-        )
-    if new_user is None:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail="Failed to update perms"
-        )
-
-    return new_user
