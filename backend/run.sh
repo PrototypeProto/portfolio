e6bf25d42a3ddcb7405f604ae4e5888a6c4c9aa9
prototypeProto <anunfoundx@gmail.com>
2026-04-10 20:25:22 -0400

    docker: updated .env vars used for testing

--- backend/run.sh
+++ backend/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 alembic upgrade head
-# exec uvicorn src:app --host 0.0.0.0 --port 8000 --reload --workers 4 # for prod
-exec uvicorn src:app --host 0.0.0.0 --port 8000 --reload
+# exec uvicorn src.app:app --host 0.0.0.0 --port 8000 --reload --workers 4 # for prod
+exec uvicorn src.app:app --host 0.0.0.0 --port 8000 --reload
